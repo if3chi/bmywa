01ba1b28829c9b4eea4f3472c3b712806928c85f
Mr Jaspel <j.cyxtus@gmail.com>
2021-06-30 20:58:13 +0000

    add metadata to head tag and .env to gitignore.

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jaspar Ifechi">
+    <meta name="description" content="Blooming Minds Young Writers Awards is an initiative aimed at raising young African leaders by recognizing and rewarding children with creative abilities in writing and arts."> 
+    <meta name="keyword" content="Blooming, Minds, arts, creatives, young, writer, creative thinking">
+    
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Blooming Minds Young Writers Award" />
+    <meta property="og:description" content="Blooming Minds Young Writers Awards is an initiative aimed at raising young African leaders..">
+    <meta property="og:image" content="{{ asset('images/24d.jpg') }}">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="200">
+    <meta property="og:url" content="{{ route('welcome') }}">
+
     <title>Blooming Minds Young Writers Award</title>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css">
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+    
     <style>
         html {
             /* padding: 0;
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'front');
+Route::view('/', 'front')->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
