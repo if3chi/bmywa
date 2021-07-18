e36ad566f012b7d4771f9a1902719169c7cb56d2
Jaspar <j.cyxtus@gmail.com>
2021-07-18 09:11:20 +0000

    Added admin prefix to url

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FrontController::class, 'index'])->name('welcome');
 Route::view('/about', 'about')->name('about');
 
+Route::group(
+    [
+        'prefix' => 'admin',
+        'middleware' => 'auth'
+    ],
+    function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+        })->name('dashboard');
+    }
+);
 
 require __DIR__.'/auth.php';
