970a9dee726f1ad6aa4252d23ff565fedf280932
Ifechi <j.cyxtus@gmail.com>
2021-09-26 23:47:51 +0000

    Updated age range for creative writing.

--- app/Utilities/Helpers.php
+++ app/Utilities/Helpers.php
@@ -15,7 +15,7 @@ if (!function_exists('entryCategories')) {
     function entryCategories(): array
     {
         return [
-            'creative-writing' => ['Creative Writing', 9, 15, 500],
+            'creative-writing' => ['Creative Writing', 10, 13, 500],
             'essay-writing' => ['Essay Writing', 14, 15, 600],
             'short-story' => ['Short Story / Poetry', 6, 9, 300]
         ];
