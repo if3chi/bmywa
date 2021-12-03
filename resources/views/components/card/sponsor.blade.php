6920ebe84f06df82a035f1f525b476820b61c064
Ifechi <j.cyxtus@gmail.com>
2021-12-03 16:26:17 +0000

    Increased Front Logo Size

--- resources/views/components/card/sponsor.blade.php
+++ resources/views/components/card/sponsor.blade.php
@@ -2,6 +2,6 @@
 
 <div class="box-border inline-block px-2 py-0 my-2 text-center text-gray-800">
     <a href="{{ $url }}" target="{{ $url ? '_blank' : '' }}" rel="noopener">
-        <img src="{{ $imgUrl }}" alt="{{ $name }}'s logo" class="block object-contain h-8 lg:h-14">
+        <img src="{{ $imgUrl }}" alt="{{ $name }}'s logo" class="block object-contain h-12 lg:h-16">
     </a>
 </div>
