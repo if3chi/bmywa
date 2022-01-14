57addece140ae53214434410f4b3e49df304c338
Jaspar <j.cyxtus@gmail.com>
2022-01-14 03:08:09 +0000

    Extended closing date

--- resources/views/livewire/front/contact-us.blade.php
+++ resources/views/livewire/front/contact-us.blade.php
@@ -85,7 +85,7 @@
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
                                     </path>
                                 </svg>
-                                <span class="ml-3">+233 (0) 54-380-7829</span>
+                                <a href="tel:+233543807829" class="ml-3">+233 (0) 54-380-7829</a>
                             </dd>
                             <dd class="flex text-base text-yellow-50">
                                 <svg class="flex-shrink-0 w-6 h-6 text-yellow-200"
@@ -95,7 +95,7 @@
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
                                     </path>
                                 </svg>
-                                <span class="ml-3">+233 (0) 55-850–5070</span>
+                                <a href="tel:+233558055070" class="ml-3">+233 (0) 55-850–5070</a>
                             </dd>
                             <dd class="flex text-base text-yellow-50">
                                 <svg class="flex-shrink-0 w-6 h-6 text-yellow-200"
@@ -105,7 +105,7 @@
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
                                     </path>
                                 </svg>
-                                <span class="ml-3">+234 (0) 906-338-8437</span>
+                                <a href="tel:+2349063388437" class="ml-3">+234 (0) 906-338-8437</a>
                             </dd>
                             <dt><span class="sr-only">Email</span></dt>
                             <dd class="flex text-base text-yellow-50">
@@ -116,7 +116,7 @@
                                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
                                     </path>
                                 </svg>
-                                <span class="ml-3">info@bmywa.com</span>
+                                <a href="mailto:info@bmywa.com" class="ml-3">info@bmywa.com</a>
                             </dd>
                         </dl>
                         <ul role="list" class="mt-8 flex space-x-12">
--- resources/views/livewire/front/entry-form.blade.php
+++ resources/views/livewire/front/entry-form.blade.php
@@ -2,7 +2,49 @@
     <div class="max-w-5xl mx-auto">
         <div class="flex flex-col py-4 md:flex-row">
 
-            <x-entry.notice />
+            <x-entry.notice>
+                <div class="py-14 px-6 text-gray-700">
+                    <h3 class="text-gray-700 text-sm font-semibold">
+                        For more information any assistance filling the form, reach us on:
+                    </h3>
+                    <dl class=" px-4 mt-8 space-y-6">
+                        <dt><span class="sr-only">Phone number</span></dt>
+                        <dd class="flex text-base text-yellow-900">
+                            <svg class="flex-shrink-0 w-5 h-5 text-yellow-900"
+                                x-description="Heroicon name: outline/phone" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                                </path>
+                            </svg>
+                            <a href="tel:+233558055070" class="ml-3 text-yellow-900 font-semibold text-sm">+233 (0)
+                                55-805-5070</a>
+                        </dd>
+                        <dt><span class="sr-only">Phone number</span></dt>
+                        <dd class="flex text-base text-yellow-900">
+                            <svg class="flex-shrink-0 w-5 h-5 text-yellow-900"
+                                x-description="Heroicon name: outline/phone" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                                </path>
+                            </svg>
+                            <a href="tel:+2349063388437" class="ml-3 text-yellow-900 font-semibold text-sm">+234 (0) 906-338-8437</a>
+                        </dd>
+                        <dt><span class="sr-only">Email</span></dt>
+                        <dd class="flex text-base text-yellow-900">
+                            <svg class="flex-shrink-0 w-5 h-5 text-yellow-900 mr-1"
+                                x-description="Heroicon name: outline/mail" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                                </path>
+                            </svg>
+                            <a href="mailto:info@bmywa.com" class="ml-3 text-yellow-900 font-semibold text-sm">info@bmywa.com</a>
+                        </dd>
+                    </dl>
+                </div>
+            </x-entry.notice>
 
             <x-entry.form wire:submit.prevent="submitEntry">
 
