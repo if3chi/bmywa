4ce7de8c35958283db243c0545094f51b643442d
Jaspar <j.cyxtus@gmail.com>
2022-03-03 20:13:14 +0000

    Added score list and made scored visible.

--- app/Http/Livewire/Admin/SubmissionsList.php
+++ app/Http/Livewire/Admin/SubmissionsList.php
@@ -6,6 +6,7 @@ use App\Models\Entry;
 use Livewire\Component;
 use App\Utilities\Constant;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class SubmissionsList extends Component
@@ -16,14 +17,22 @@ class SubmissionsList extends Component
     public $activeEntry;
     public $formTitle;
     public $entryScore;
+    public $scoredList;
     public $showScoreModal = false;
+    public $showScoredList = false;
 
-    protected $rules = ['entryScore' => 'required|numeric|max:100'];
+    protected $rules = ['entryScore' => 'required|numeric|max:50'];
+
+    public function mount()
+    {
+        $this->scoredList = collect();
+    }
 
     public function openEntry(Entry $entry)
     {
         $this->activeEntry = $entry->id;
         $this->readingView = $entry;
+        $this->showScoredList = false;
     }
 
     public function scoreEntry()
@@ -42,10 +51,23 @@ class SubmissionsList extends Component
         $validScore = $this->validate()['entryScore'];
 
         $this->readingView->setAttribute('score', $validScore)->save();
+        Cache::forget('scoredList');
 
         $this->notify(['title' => 'Success!']);
         $this->reset('readingView', 'activeEntry');
         $this->showScoreModal = false;
+    }
+
+    public function getScoredList()
+    {
+        $this->showScoredList = true;
+        $this->scoredList = Cache::remember('scoredList', now()->addDays(2), function () {
+            return Entry::where('score', '!=', null)->orderBy('updated_at', 'desc')->get();
+        });
+
+        $listCount = count($this->scoredList);
+
+        $this->formTitle = "$listCount Scored Entries";
     }
 
     public function loadData()
@@ -53,7 +75,7 @@ class SubmissionsList extends Component
         $data = collect();
 
         if (Gate::allows(Constant::SCORE_ENTRY)) {
-            $data = Entry::where('score', null)->get();
+            $data = Entry::orderBy('score', 'asc')->get();
         }
 
         return $data;
---
+++ resources/views/components/entry/list.blade.php
@@ -0,0 +1,54 @@
+@props(['entries', 'activeEntry'])
+
+<div class="flex-auto overflow-y-auto">
+    <div>
+        @forelse ($entries as $entry)
+            @php
+                if ($entry->score && $activeEntry == $entry->id) {
+                    $cardClasses = 'border-green-400 bg-green-200 hover:bg-green-300';
+                } elseif ($entry->score) {
+                    $cardClasses = 'bg-green-100 hover:bg-green-300';
+                } else {
+                    $cardClasses = $activeEntry == $entry->id ? 'border-yellow-400 bg-yellow-100 hover:bg-yellow-200' : 'bg-yellow-50 hover:bg-yellow-200';
+                }
+            @endphp
+
+            <a href="#" class="block border-b" wire:click="openEntry({{ $entry->id }})">
+                <div class="border-l-2 {{ $cardClasses }} p-3 space-y-1">
+                    <div class="flex flex-row items-center space-x-2 text-gray-700">
+                        <strong class="flex-grow text-sm truncate">{{ $entry->contestant_name }}</strong>
+                        <div class="flex space-x-1">
+                            <div class="text-sm font-medium text-gray-700">{{ $entry->age }}yrs
+                            </div>
+                            <div
+                                class="text-sm {{ $entry->country == 'ng' ? 'text-green-800 bg-green-200' : 'text-yellow-800 bg-yellow-300' }}  p-1 -m-1 rounded-full">
+                                {{ strtoupper($entry->country) }}
+                            </div>
+                            @if ($entry->score)
+                                <div class="mx-1">
+                                    | <span class="bg-green-700 text-white px-0.5 rounded-sm">
+                                        {{ $entry->score }}
+                                    </span>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row items-center space-x-1 text-gray-600">
+                        <div class="flex-grow truncate text-xs font-medium tracking-wide">
+                            {{ $entry->title }} </div>
+                        <div class="truncate text-xs font-medium tracking-wide">{{ $entry->category }} </div>
+                    </div>
+
+                    <div class="flex flex-row items-center space-x-1 text-gray-600">
+                        <div class="flex-grow truncate text-xs">{{ $entry->truncated }}</div>
+                    </div>
+                </div>
+            </a>
+        @empty
+            <h2 class="text-center text-xl my-10 font-semibold tracking-wide text-gray-700 dark:text-gray-200">
+                Nothing Here...
+            </h2>
+        @endforelse
+    </div>
+</div>
